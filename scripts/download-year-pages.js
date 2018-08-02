ffd09ffa4a657d66ab5b3a79b3061720c5ccc13e
Caitlyn Ralph <caitlynr@catlyn-macbookpro.roam.corp.google.com>
2018-08-02 11:50:56 -0400

    get year pages

---
+++ scripts/download-year-pages.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const d3 = require('d3');
+const mkdirp = require('mkdirp');
+const wiki = require('wikijs').default;
+
+const START_YEAR = 1900
+const date = new Date()
+const END_YEAR = date.getFullYear()
+
+const years = d3.range(START_YEAR,END_YEAR+1)
+
+const outputDir = './output/year-pages';
+
+function download(year) {
+    return new Promise((resolve, reject) => {
+        wiki()
+            .page(year)
+            .then(page => page.html())
+            .then(response => {
+                fs.writeFileSync(`${outputDir}/${year}.html`, response)
+                resolve()
+            })
+            .catch(reject)
+    })
+}
+
+async function init() {
+    mkdirp(outputDir);
+  
+    let i = 0;
+    for (const year of years) {
+      await download(year)
+        .then(() => console.log(`${i} of ${years.length}`))
+        .catch(console.error);
+      i += 1;
+    }
+  }
+
+
+init()
